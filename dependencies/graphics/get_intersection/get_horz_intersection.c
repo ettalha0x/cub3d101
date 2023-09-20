7b646afc464c578bafc1e77e35b7231149aa1f84
Oussama Kamili <oussama.kamili@protonmail.com>
2023-09-20 04:59:56 +0100

    spliting get_horz_intersection to two, to help with norminette

--- dependencies/graphics/get_intersection/get_horz_intersection.c
+++ dependencies/graphics/get_intersection/get_horz_intersection.c
@@ -6,17 +6,44 @@
 /*   By: okamili <okamili@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 15:59:27 by nettalha          #+#    #+#             */
-/*   Updated: 2023/09/20 03:56:26 by okamili          ###   ########.fr       */
+/*   Updated: 2023/09/20 04:55:18 by okamili          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../graphics.h"
 
+void	get_ray_distance(t_data *data, t_ray *ray, int depth, double angle)
+{
+	int		grid_x;
+	int		grid_y;
+	double	ystep;
+	double	xstep;
+
+	ystep = BLOCK_SIZE;
+	if (ray->is_facing_up)
+		ystep *= -1;
+	xstep = ystep / tan(angle * (M_PI / 180));
+	while (depth > 0 && ray->x >= 0 && ray->x < (data->map_w * BLOCK_SIZE)
+		&& ray->y >= 0 && ray->y < (data->map_h * BLOCK_SIZE))
+	{
+		grid_x = floor(ray->x / BLOCK_SIZE);
+		grid_y = floor(ray->y / BLOCK_SIZE);
+		if (grid_y < 0 || grid_y >= data->map_h || grid_x < 0
+			|| grid_x >= (int)ft_strlen(data->map[grid_y])
+			|| data->map[grid_y][grid_x] != '0')
+			break ;
+		else
+		{
+			ray->x += xstep;
+			ray->y += ystep;
+		}
+		depth--;
+	}
+}
+
 t_ray	get_horz_intersection(t_data *data, double angle)
 {
 	t_ray		ray;
-	double		ystep;
-	double		xstep;
 	int			depth;
 	t_coords	temp_player;
 
@@ -35,28 +62,7 @@ t_ray	get_horz_intersection(t_data *data, double angle)
 	if (ray.is_facing_up)
 		ray.y -= 0.0001;
 	ray.x = temp_player.x + (ray.y - temp_player.y) / tan(angle * (M_PI / 180));
-	ystep = BLOCK_SIZE;
-	if (ray.is_facing_up)
-		ystep *= -1;
-	xstep = ystep / tan(angle * (M_PI / 180));
-	int grid_x, grid_y;
-	while (depth > 0 && ray.x >= 0 && ray.x < (data->map_w * BLOCK_SIZE)
-		&& ray.y >= 0 && ray.y < (data->map_h * BLOCK_SIZE))
-	{
-		grid_x = floor(ray.x / BLOCK_SIZE);
-		grid_y = floor(ray.y / BLOCK_SIZE);
-		if (grid_y < 0 || grid_y >= data->map_h || grid_x < 0
-			|| grid_x >= (int)ft_strlen(data->map[grid_y]))
-			break ;
-		if (data->map[grid_y][grid_x] != '0')
-			break ;
-		else
-		{
-			ray.x += xstep;
-			ray.y += ystep;
-		}
-		depth--;
-	}
+	get_ray_distance(data, &ray, depth, angle);
 	ray.distance = sqrt(pow(ray.x - temp_player.x, 2)
 			+ pow(ray.y - temp_player.y, 2));
 	return (ray);
